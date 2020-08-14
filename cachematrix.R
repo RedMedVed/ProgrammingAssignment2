c8a495032ceec5fe2157aba09cdbfb42769d6881
Sukharkov Maxim <suharkov.maxim@gmail.com>
2020-08-14 10:16:39 +0300

    Programming Assignment 2: Lexical Scoping Workable function comes here

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions is used to save time while reversing large matrices (using cache)
 
-## Write a short comment describing this function
+## Remember, that the matrix passed as argument must be invertible and square - I don't want to
+## import any libraries now. In other cases function will raise errors:
+## 1) Lapack routine dgesv: system is exactly singular
+## 2) Error in solve.default(data, ...) : 'a' (ncol x nrow) must be square
+## I tested this using matrix(rnorm(), ncol = 4, nrow = 4) in order to see if all works.
+
+## This function caches the reverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## This function returns cached reverse if exist, else caches
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m
 }
